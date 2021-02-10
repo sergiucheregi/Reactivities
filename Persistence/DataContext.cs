6e96a2da50c78186a1259379630690eec14ce3d7
Sergiu C <sergiu.cheregi@yahoo.com>
2021-02-10 08:40:52 +0200

    changed Value to 109

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -18,7 +18,7 @@ namespace Persistence
         {
             builder.Entity<Value>()
                 .HasData(
-                new Value {Id = 1, Name = "value 102"},
+                new Value {Id = 1, Name = "value 109"},
                 new Value {Id = 2, Name = "value 102"},
                 new Value {Id = 3, Name = "value 103"}
             );
